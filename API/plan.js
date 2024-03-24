be855b4cab090410c24abae9f4863121116e4cf0
BillGray <junjason1126@gmail.com>
2024-03-24 11:45:49 -0700

    plan subject --> workout

--- API/plan.js
+++ API/plan.js
@@ -4,11 +4,8 @@ const pool = require("../model/pool");
 const redisClient = require("../model/redis");
 const { isValidJSON, hashing, generateRandomId, autoSignin, googleOauth2client } = require("../tool");
 const { removePrevNotification, planNotification, planPushNotification } = require("../services/notification");
-//const { google } = require('googleapis');
 const { DateTime } = require("luxon");
-const { UserRefreshClient } = require("google-auth-library");
 const { validateStrictString, validateInteger, validateLength, validateString } = require("../validate");
-//const { googleAccessTokenCache } = require("../services/redisLoader");
 const schedule = require('node-schedule');
 
 
@@ -16,7 +13,7 @@ Router.get("/", async (req, res) => {
   autoSignin(req, res, (async (userId) => {
     try {
       const connection = pool.promise();
-      let [plans] = await connection.query(`SELECT id, title, start, end, \`repeat\`, description, notification, subject, priority, completed FROM plans where user_id = ?`, [userId]);
+      let [plans] = await connection.query(`SELECT id, title, start, end, \`repeat\`, description, notification, workout, priority, completed FROM plans where user_id = ?`, [userId]);
       plans.map(plan => {
         plan.editable = true;
         plan.isEditable = true;
@@ -39,51 +36,7 @@ Router.post('/update', async (req, res) => {
 
       const minPlanTime = DateTime.now().minus({ month: 1 }).toSeconds() / 60;
       const maxPlanTime = DateTime.now().plus({ year: 1 }).toSeconds() / 60;
-      const { title, id, start, end, repeat, description, subject, notification, priority, completed, type } = planInfo;
-
-
-      console.log(type);
-      if (type === "google") {
-        const access_token = await googleAccessTokenCache(userId);
-        if (access_token) {
-          try {
-            const auth = googleOauth2client({ access_token });
-            const googleCalendar = google.calendar({
-              version: 'v3',
-              auth: auth
-            });
-
-
-            const startDateTime = DateTime.fromSeconds(start * 60, { zone: timezone });
-            const endDateTime = DateTime.fromSeconds(end * 60, { zone: timezone });
-
-
-            console.log(subject, id)
-            const updateResults = await googleCalendar.events.update({
-              auth: auth,
-              calendarId: subject,
-              eventId: id,
-              resource: {
-                summary: title,
-                description,
-                start: { dateTime: startDateTime.toISO(), timeZone: timezone },
-                end: { dateTime: endDateTime.toISO(), timeZone: timezone },
-              }
-            });
-
-
-            if (updateResults.status === 200) {
-              return res.send({ success: true, msg: "Plan updated!" });
-            } else {
-              return res.send({ success: false, msg: "You cannot modify this plan" });
-            }
-          } catch (err) {
-
-
-          }
-        };
-      };
-
+      const { title, id, start, end, repeat, description, workout, notification, priority, completed, type } = planInfo;
 
       const isValidTitle = validateString(title, 'Title', 100);
       if (!isValidTitle.isValid) {
@@ -94,58 +47,49 @@ Router.post('/update', async (req, res) => {
         return res.send({ success: false, reason: isValidId.reason });
       };
 
-
       const isValidStart = validateInteger(start, 'Start time', maxPlanTime, minPlanTime);
       if (!isValidStart.isValid) {
         return res.send({ success: false, reason: isValidStart.reason });
       };
-
 
       const isValidEnd = validateInteger(end, 'End time', maxPlanTime, start);
       if (!isValidEnd.isValid) {
         return res.send({ success: false, reason: isValidEnd.reason });
       };
 
-
       const isValidRepeat = validateInteger(repeat, 'Repeat', 3, 0);
       if (!isValidRepeat.isValid) {
         return res.send({ success: false, reason: isValidRepeat.reason });
       };
-
 
       const isValidDescription = validateLength(description, 'Description', 300);
       if (!isValidDescription.isValid) {
         return res.send({ success: false, reason: isValidDescription.reason });
       };
 
-
-      const isValidSubject = validateStrictString(subject, 'Subject', 10, 10);
-      if (!isValidSubject.isValid) {
-        return res.send({ success: false, reason: isValidSubject.reason });
+      const isValidWorkout = validateStrictString(workout, 'Workout', 10, 10);
+      if (!isValidWorkout.isValid) {
+        return res.send({ success: false, reason: isValidWorkout.reason });
       };
-
 
       const isValidNotification = validateInteger(notification, 'Notification', -1, 60);
       if (!isValidNotification.isValid) {
         return res.send({ success: false, reason: isValidNotification.reason });
       };
 
-
-      const isValidPriority = validateStrictString(priority, 'Subject', 10, 10);
+      const isValidPriority = validateStrictString(priority, 'Workout', 10, 10);
       if (!isValidPriority.isValid) {
         return res.send({ success: false, reason: isValidPriority.reason });
       };
-
 
       const isValidCompleted = validateInteger(completed, 'Completed', 0, 1);
       if (!isValidCompleted.isValid) {
         return res.send({ success: false, reason: isValidCompleted.reason });
       };
 
-
       try {
         const connection = pool.promise();
-        const planData = { title, id, start, end, repeat, description, subject, notification, priority, completed };
+        const planData = { title, id, start, end, repeat, description, workout, notification, priority, completed };
         const insertInfo = { ...planData, user_id: userId };
         const [deletePrev] = await connection.query(`DELETE FROM plans WHERE user_id = ? AND id = ?`, [userId, planData.id]);
         if (!deletePrev.affectedRows) {
@@ -153,7 +97,6 @@ Router.post('/update', async (req, res) => {
         }
         const [[userInfo]] = await connection.query(`SELECT key_salt, iv, notification_endpoint, notification_keys from users where user_id = ?`, [userId]);
         const startTime = start * 60;
-        //const body = description.replace(/(&nbsp;|<([^>]+)>)/ig, " ");
         const startDateTime = DateTime.fromSeconds(startTime).setZone(timezone).toFormat("h:mm a");
         const endDateTime = DateTime.fromSeconds(end * 60).setZone(timezone).toFormat("h:mm a")
         const body = `${startDateTime} - ${endDateTime}`;
@@ -183,6 +126,7 @@ Router.post('/update', async (req, res) => {
         //planNotification(insertInfo, userInfo[0], startTime)
         const insert = await connection.query(`INSERT INTO plans SET ?`, insertInfo);
         res.send({ success: true, msg: 'Plan Saved!' })
+
       } catch (error) {
         res.send({ success: false, reason: 'An error occurred' });
         console.log('Mysql Err', error);
@@ -200,22 +144,17 @@ Router.post("/status-change", async (req, res) => {
     try {
       const { id, completed } = req.body;
 
-
       const isValidId = validateStrictString(id, 'plan id', 10, 8);
-
 
       if (!isValidId) {
         return res.send({ success: false, reason: isValidId.reason });
       };
 
-
       const isValidCompleted = validateInteger(completed, 'completed', 1, 0);
-
 
       if (!isValidCompleted) {
         return res.send({ success: false, reason: isValidCompleted.reason });
       };
-
 
       const connection = pool.promise();
       try {
